06d1b6d37ce78d1b0890a95c9eb1206e08b3564a
YuvalBenAmi <yuvalba2005@gmail.com>
2024-10-16 15:36:17 +0300

    Edited ItemOfWrongType

--- LinkedList/ItemOfWrongeTypeException.cs
+++ LinkedList/ItemOfWrongeTypeException.cs
@@ -6,6 +6,8 @@ namespace LinkedList
 {
     class ItemOfWrongeTypeException : SystemException
     {
-
+        public ItemOfWrongeTypeException(): base("The item is of the wrong type.") {}
+        public IndexOutOfRangeException(string? message): base(message) {}
+        public IndexOutOfRangeException(string? message, Exception? innerException): base(message, innerException) {}
     }
 }
