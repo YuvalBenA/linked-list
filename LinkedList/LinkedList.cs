1cb689d6706f340c7d9333b7e302c46c725ae1e8
YuvalBenAmi <yuvalba2005@gmail.com>
2024-10-16 14:02:21 +0300

    Initial commit

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class LinkedList<T> : IList<T>
+    {
+        public Node<T> Head { get; }
+        public Node<T> Last { get; }
+        public T this[int index]
+        {
+            get
+            {
+
+            }
+            set
+            {
+
+            }
+        }
+
+        public int Count { get; }
+
+        public bool IsReadOnly { get; }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
