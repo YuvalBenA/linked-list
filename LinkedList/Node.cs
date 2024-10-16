0f8d1866103a3ba4234153988e3c0961134e5ab3
YuvalBenAmi <yuvalba2005@gmail.com>
2024-10-16 14:11:29 +0300

    Create basic Node class

--- LinkedList/Node.cs
+++ LinkedList/Node.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace LinkedList
 {
-    class Node
+    class Node<T>
     {
+        public T Value {get; set;}
+        
+        public Node<T> Next {get; set;}
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+        
+        public Node(T value, Node<T> next)
+        {
+            Value = value;
+            Next = next;
+        }
     }
 }
